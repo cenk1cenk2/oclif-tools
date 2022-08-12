93749c5ef9adc12dfe995c8513210e5489cd92cf
Cenk Kılıç <cenk@kilic.dev>
2022-08-12 22:00:32 +0200

    feat: add sync counterparts for fs-extra jumpers

--- packages/common/src/lib/fs/filesystem.service.ts
+++ packages/common/src/lib/fs/filesystem.service.ts
@@ -45,6 +45,18 @@ export class FileSystemService {
     }
   }
 
+  public readSync (file: string): string {
+    try {
+      const raw = fs.readFileSync(file, 'utf-8')
+
+      return raw
+    } catch (e) {
+      this.logger.fatal('Error while reading file from "%s": %s', file, e.message)
+
+      throw e
+    }
+  }
+
   public async write (file: string, data: string | Buffer, options: fs.WriteFileOptions = {}): Promise<void> {
     try {
       await fs.writeFile(file, data, { encoding: 'utf-8', ...options })
@@ -55,9 +67,29 @@ export class FileSystemService {
     }
   }
 
+  public writeSync (file: string, data: string | Buffer, options: fs.WriteFileOptions = {}): void {
+    try {
+      fs.writeFileSync(file, data, { encoding: 'utf-8', ...options })
+    } catch (e) {
+      this.logger.fatal('Error while writing file to "%s": %s', file, e.message)
+
+      throw e
+    }
+  }
+
   public async append (file: string, data: string | Buffer): Promise<void> {
     try {
       await fs.appendFile(file, data)
+    } catch (e) {
+      this.logger.fatal('Error while appending file to "%s": %s', file, e.message)
+
+      throw e
+    }
+  }
+
+  public appendSync (file: string, data: string | Buffer): void {
+    try {
+      fs.appendFileSync(file, data)
     } catch (e) {
       this.logger.fatal('Error while appending file to "%s": %s', file, e.message)
 
@@ -77,6 +109,18 @@ export class FileSystemService {
     throw new Error('Not implemented!')
   }
 
+  public removeSync (file: string): void {
+    const stats = this.stats(file)
+
+    if (stats.isFile()) {
+      return this.removeFileSync(file)
+    } else if (stats.isDirectory()) {
+      return this.removeDirectorySync(file)
+    }
+
+    throw new Error('Not implemented!')
+  }
+
   public async removeFile (file: string): Promise<void> {
     try {
       if (this.stats(file).isFile()) {
@@ -91,10 +135,38 @@ export class FileSystemService {
     }
   }
 
+  public removeFileSync (file: string): void {
+    try {
+      if (this.stats(file).isFile()) {
+        fs.unlinkSync(file)
+      } else {
+        throw new Error('Not a file.')
+      }
+    } catch (e) {
+      this.logger.fatal('Error while deleting file from "%s": %s', file, e.message)
+
+      throw e
+    }
+  }
+
   public async removeDirectory (directory: string): Promise<void> {
     try {
       if (this.stats(directory).isDirectory()) {
         await fs.emptyDir(directory)
+      } else {
+        throw new Error('Not a folder.')
+      }
+    } catch (e) {
+      this.logger.fatal('Error while deleting directory from "%s": %s', directory, e.message)
+
+      throw e
+    }
+  }
+
+  public removeDirectorySync (directory: string): void {
+    try {
+      if (this.stats(directory).isDirectory()) {
+        fs.emptyDirSync(directory)
       } else {
         throw new Error('Not a folder.')
       }
