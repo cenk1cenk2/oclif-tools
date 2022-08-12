9ea9259095617b2290beea79a4ae9d00213443bd
Cenk Kılıç <cenk@kilic.dev>
2022-08-13 01:49:00 +0200

    fix: update init hook

--- packages/common/src/hooks/init.hook.ts
+++ packages/common/src/hooks/init.hook.ts
@@ -2,14 +2,6 @@ import type { Hook } from '@oclif/core'
 
 import { setup } from '@lib'
 
-export const initHook: Hook<'init'> = async (opts) => {
+export const initHook: Hook<'init'> = async () => {
   setup()
-
-  if ((opts.config.pjson.oclif as any)?.defaultCommand && !opts.id) {
-    try {
-      await opts.config.runCommand((opts.config.pjson.oclif as any).defaultCommand)
-    } finally {
-      process.exit(0)
-    }
-  }
 }
