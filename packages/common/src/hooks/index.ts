d0adde414ef710ee416f3347def6e826c675f3ba
Cenk Kılıç <cenk@kilic.dev>
2022-08-13 02:10:01 +0200

    fix: update basic configuration

--- packages/common/src/hooks/index.ts
+++ packages/common/src/hooks/index.ts
@@ -1,3 +1,2 @@
-export * from './init.hook'
 export * from './not-found.hook'
 export * from './update-notifier.hook'
--- packages/common/src/hooks/init.hook.ts
+++
@@ -1,7 +0,0 @@
-import type { Hook } from '@oclif/core'
-
-import { setup } from '@lib'
-
-export const initHook: Hook<'init'> = async () => {
-  setup()
-}
