c3a5fce7304c3d0ae41cc584e5b69528125360e4
Cenk Kılıç <cenk@kilic.dev>
2023-04-08 14:08:59 +0200

    fix: update update-notifier dependency injection

--- packages/common/src/hooks/update-notifier.hook.ts
+++ packages/common/src/hooks/update-notifier.hook.ts
@@ -1,6 +1,7 @@
 import type { Hook } from '@oclif/core'
-import notifier from 'update-notifier'
 
 export const updateNotifierHook: Hook<'init'> = async (opts) => {
+  const { default: notifier } = await import('update-notifier')
+
   notifier({ pkg: { name: opts.config.name, version: opts.config.version } }).notify({ isGlobal: true })
 }
