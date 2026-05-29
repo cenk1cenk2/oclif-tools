ed0ad889c7a4101968421d2e637fa70e00111243
Cenk Kılıç <cenk@kilic.dev>
2026-05-29 13:27:25 +0200

    build(ts): migrate oclif-tools to TypeScript 6
    Refs K-326

--- packages/common/src/interfaces/oclif.interface.ts
+++ packages/common/src/interfaces/oclif.interface.ts
@@ -3,7 +3,6 @@ import type { Command, Interfaces } from '@oclif/core'
 export type InferFlags<T extends typeof Command> = Interfaces.InferredFlags<(typeof Command)['baseFlags'] & T['flags']>
 export type InferArgs<T extends typeof Command> = Interfaces.InferredArgs<T['args']>
 
-export type { Flag, InferredFlags } from '@oclif/core/lib/interfaces'
-export type { ArgInput, FlagInput, Arg } from '@oclif/core/lib/interfaces/parser'
+export type { Arg, ArgInput, Flag, FlagInput, InferredFlags } from '@oclif/core/interfaces'
 
 export { Flags, Args } from '@oclif/core'
