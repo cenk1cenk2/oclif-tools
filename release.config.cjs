daa97595026ad6512bc7b9a5a202f58af5f0e78d
Labrat <labrat@kilic.dev>
2026-09-10 20:24:24 +0200

    chore(release): migrate to the shared preset and anolilab multi-semantic-release (K-1292, K-1329)

--- release.config.cjs
+++ release.config.cjs
@@ -1,44 +1,4 @@
 module.exports = {
-  branches: [
-    'main',
-    'master',
-    'next',
-    'next-major',
-    {
-      name: 'alpha',
-      prerelease: true
-    },
-    {
-      name: 'beta',
-      prerelease: true
-    },
-    {
-      name: 'rc',
-      prerelease: true
-    }
-  ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'README.md', 'docs/']
-      }
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false
-      }
-    ],
-    [
-      '@semantic-release/exec',
-      {
-        publishCmd:
-          "npm stage publish ${nextRelease.channel ? \"--tag \" + nextRelease.channel : \"\"} > /dev/null && node -p \"JSON.stringify({ name: 'npm package', url: 'https://www.npmjs.com/package/' + require('./package.json').name + '/v/${nextRelease.version}', channel: '${nextRelease.channel || \"latest\"}' })\""
-      }
-    ]
-  ]
+  extends: '@cenk1cenk2/semantic-release-config',
+  plugins: [['@cenk1cenk2/semantic-release-config/presets/npm', { client: 'pnpm', publish: 'staged' }]]
 }
